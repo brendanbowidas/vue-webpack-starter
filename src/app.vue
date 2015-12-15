38fa0c20faaed9696e4de47536389cdad0533025
Brendan Bowidas <brendan@machine.andrew.response.private>
2015-12-15 10:30:51 -0500

    initial commit

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<template>
+    <p class="red">{{msg}}</p>
+
+</template>
+
+
+<style lang="sass">
+
+</style>
+
+
+<script>
+    require('./scss/main.scss');
+
+
+    export default {
+
+        data() {
+                return {
+                    msg: 'hello'
+                }
+            },
+
+            components: {
+
+
+            }
+
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+    import App from './app.vue'
+
+
+        Vue.use(require('vue-resource'));
+
+new Vue({
+    el: 'body',
+    components: {
+        app: App
+    }
+});
